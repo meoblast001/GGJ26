7c90c0702d82874c6829b2f912ae207c5cd08a2c
Braden Walters <vc@braden-walters.info>
2026-01-31 19:30:53 +0100

    Setup scene switching.

--- Assets/Scripts/StartConsoleBehavior.cs
+++ Assets/Scripts/StartConsoleBehavior.cs
@@ -1,9 +1,29 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartConsoleBehavior : MonoBehaviour
 {
+    [SerializeField] private GameObject waitingForClientDisplay;
+    [SerializeField] private string gameplaySceneName;
+
     void Start()
     {
-        Debug.Log("Starting");
+        StartCoroutine(AwaitConnection());
+    }
+
+    private IEnumerator AwaitConnection()
+    {
+        Debug.Log("Waiting for client to connect");
+        yield return new WaitForSeconds(3);
+        //Wait for client. Then..
+        OnClientConnected();
+    }
+
+    private void OnClientConnected()
+    {
+        Debug.Log("Client connected");
+        waitingForClientDisplay.SetActive(false);
+        SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
     }
 }
