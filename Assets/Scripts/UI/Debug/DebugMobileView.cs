89cbce201d32a534d0cbcab0f257b1815fba1217
Jose Contreras <nero.lucho92@gmail.com>
2026-01-31 12:17:19 +0100

    Add debug UI for testing messages and TMP assets

---
+++ Assets/Scripts/UI/Debug/DebugMobileView.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMobileView : MonoBehaviour
+{
+    public Button SendCommandButton;
+    public TextMeshProUGUI ReceiveMessageText;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
